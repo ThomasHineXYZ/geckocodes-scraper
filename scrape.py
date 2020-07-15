1fbf9a98b8f4abfa4de6fd0fbe5aaa6696fccb56
Thomas <guitaristtom@users.noreply.github.com>
2020-07-15 05:01:33 -0700

    Uncommented the different urls, need to test that they all work properly with this. Removed all print commands. Got it storing in a compressed tar.gz archive.

--- scrape.py
+++ scrape.py
@@ -1,73 +1,75 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup;
+import os;
 import requests;
+import tarfile;
 import tempfile;
 import time;
 import urllib.request
 
 # URLs to scrape
 urls = {
-    # 'Wii': {
-    #     'folder': 'wii',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=R&r=*&l=all'
-    # },
-    # 'WiiWare': {
-    #     'folder': 'wii_ware',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=W&r=*&l=all' # NOTE Commented all of this out for testing purposes
-    # },
-    # 'Virtual Console Arcade': {
-    #     'folder': 'virtual_console_arcade',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=D&r=*&l=all'
-    # },
+    'Wii': {
+        'fileName': 'wii',
+        'url': 'https://www.geckocodes.org/index.php?chid=R&r=*&l=all'
+    },
+    'WiiWare': {
+        'fileName': 'wii_ware',
+        'url': 'https://www.geckocodes.org/index.php?chid=W&r=*&l=all'
+    },
+    'Virtual Console Arcade': {
+        'fileName': 'virtual_console_arcade',
+        'url': 'https://www.geckocodes.org/index.php?chid=D&r=*&l=all'
+    },
     'Wii Channels': {
-        'folder': 'wii_channels',
+        'fileName': 'wii_channels',
         'url': 'https://www.geckocodes.org/index.php?chid=H&r=*&l=all'
     },
-    # 'Gamecube': {
-    #     'folder': 'gamecube',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=G&r=*&l=all'
-    # },
-    # 'NES / Famicom': {
-    #     'folder': 'n_e_s_famicom',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=F&r=*&l=all'
-    # },
-    # 'Super NES/Famicom': {
-    #     'folder': 'super_n_e_s_famicom',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=J&r=*&l=all'
-    # },
-    # 'Nintendo 64': {
-    #     'folder': 'nintendo_64',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=N&r=*&l=all'
-    # },
-    # 'Sega Master System': {
-    #     'folder': 'sega_master_system',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=L&r=*&l=all'
-    # },
-    # 'Genesis/Mega Drive': {
-    #     'folder': 'genesis_mega_drive',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=M&r=*&l=all'
-    # },
-    # 'NeoGeo': {
-    #     'folder': 'neo_geo',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=E&r=*&l=all'
-    # },
-    # 'Commodore 64': {
-    #     'folder': 'commodore_64',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=C&r=*&l=all'
-    # },
-    # 'MSX': {
-    #     'folder': 'msx',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=X&r=*&l=all'
-    # },
-    # 'TurboGraFX-16': {
-    #     'folder': 'turbo_gra_f_x_16',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=P&r=*&l=all'
-    # },
-    # 'TurboGraFX-CD': {
-    #     'folder': 'turbo_gra_f_x_c_d',
-    #     'url': 'https://www.geckocodes.org/index.php?chid=Q&r=*&l=all'
-    # },
+    'Gamecube': {
+        'fileName': 'gamecube',
+        'url': 'https://www.geckocodes.org/index.php?chid=G&r=*&l=all'
+    },
+    'NES / Famicom': {
+        'fileName': 'n_e_s_famicom',
+        'url': 'https://www.geckocodes.org/index.php?chid=F&r=*&l=all'
+    },
+    'Super NES/Famicom': {
+        'fileName': 'super_n_e_s_famicom',
+        'url': 'https://www.geckocodes.org/index.php?chid=J&r=*&l=all'
+    },
+    'Nintendo 64': {
+        'fileName': 'nintendo_64',
+        'url': 'https://www.geckocodes.org/index.php?chid=N&r=*&l=all'
+    },
+    'Sega Master System': {
+        'fileName': 'sega_master_system',
+        'url': 'https://www.geckocodes.org/index.php?chid=L&r=*&l=all'
+    },
+    'Genesis/Mega Drive': {
+        'fileName': 'genesis_mega_drive',
+        'url': 'https://www.geckocodes.org/index.php?chid=M&r=*&l=all'
+    },
+    'NeoGeo': {
+        'fileName': 'neo_geo',
+        'url': 'https://www.geckocodes.org/index.php?chid=E&r=*&l=all'
+    },
+    'Commodore 64': {
+        'fileName': 'commodore_64',
+        'url': 'https://www.geckocodes.org/index.php?chid=C&r=*&l=all'
+    },
+    'MSX': {
+        'fileName': 'msx',
+        'url': 'https://www.geckocodes.org/index.php?chid=X&r=*&l=all'
+    },
+    'TurboGraFX-16': {
+        'fileName': 'turbo_gra_f_x_16',
+        'url': 'https://www.geckocodes.org/index.php?chid=P&r=*&l=all'
+    },
+    'TurboGraFX-CD': {
+        'fileName': 'turbo_gra_f_x_c_d',
+        'url': 'https://www.geckocodes.org/index.php?chid=Q&r=*&l=all'
+    },
 };
 
 txtDownloadUrl = "https://www.geckocodes.org/txt.php?txt=";
@@ -81,7 +83,6 @@ for url in urls:
 
     # Create a temporary directory
     tempDirectory = tempfile.TemporaryDirectory();
-    print(tempDirectory); # NOTE Maybe remove this later?
 
     # Iterate through each of the games listed
     for link in soup.find("div", class_="list").findAll("a"):
@@ -91,14 +92,14 @@ for url in urls:
         # Chop off the index.php part
         titleId = cheatLink.replace("./index.php?c=", "");
 
-        # NOTE Print it for good measure. Might remove this later.
-        print(txtDownloadUrl + titleId);
-
         # Finally save the resulting text file
         response = urllib.request.urlretrieve(txtDownloadUrl + titleId, tempDirectory.name + "/" + titleId + ".txt");
-        time.sleep(0.1);
 
-    # NOTE need to add in compression and more "permanent" storage of the files
+    # Put all of the newly downloaded files in to a tar.gz archive file
+    tar = tarfile.open(urls[url]['fileName'] + ".tar.gz", "w:gz");
+    for name in os.listdir(tempDirectory.name):
+        tar.add(tempDirectory.name + "/" + name, arcname=name);
+    tar.close();
 
     # Close the temp directory to clean up the files
     tempDirectory.cleanup();
