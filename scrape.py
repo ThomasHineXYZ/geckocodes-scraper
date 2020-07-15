1f1d28414e29d63d960f7083ed78f609ebfd9052
Thomas <guitaristtom@users.noreply.github.com>
2020-07-15 11:22:13 -0700

    Removed the need for the requrests library to be used.

--- scrape.py
+++ scrape.py
@@ -2,7 +2,6 @@
 
 from bs4 import BeautifulSoup;
 import os;
-import requests;
 import tarfile;
 import tempfile;
 import time;
@@ -10,76 +9,76 @@ import urllib.request
 
 # URLs to scrape
 urls = {
-    'Wii': {
-        'fileName': 'wii',
-        'url': 'https://www.geckocodes.org/index.php?chid=R&r=*&l=all'
-    },
-    'WiiWare': {
-        'fileName': 'wii_ware',
-        'url': 'https://www.geckocodes.org/index.php?chid=W&r=*&l=all'
-    },
-    'Virtual Console Arcade': {
-        'fileName': 'virtual_console_arcade',
-        'url': 'https://www.geckocodes.org/index.php?chid=D&r=*&l=all'
-    },
+    # 'Wii': {
+    #     'fileName': 'wii',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=R&r=*&l=all'
+    # },
+    # 'WiiWare': {
+    #     'fileName': 'wii_ware',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=W&r=*&l=all'
+    # },
+    # 'Virtual Console Arcade': {
+    #     'fileName': 'virtual_console_arcade',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=D&r=*&l=all'
+    # },
     'Wii Channels': {
         'fileName': 'wii_channels',
         'url': 'https://www.geckocodes.org/index.php?chid=H&r=*&l=all'
     },
-    'Gamecube': {
-        'fileName': 'gamecube',
-        'url': 'https://www.geckocodes.org/index.php?chid=G&r=*&l=all'
-    },
-    'NES / Famicom': {
-        'fileName': 'n_e_s_famicom',
-        'url': 'https://www.geckocodes.org/index.php?chid=F&r=*&l=all'
-    },
-    'Super NES/Famicom': {
-        'fileName': 'super_n_e_s_famicom',
-        'url': 'https://www.geckocodes.org/index.php?chid=J&r=*&l=all'
-    },
-    'Nintendo 64': {
-        'fileName': 'nintendo_64',
-        'url': 'https://www.geckocodes.org/index.php?chid=N&r=*&l=all'
-    },
-    'Sega Master System': {
-        'fileName': 'sega_master_system',
-        'url': 'https://www.geckocodes.org/index.php?chid=L&r=*&l=all'
-    },
-    'Genesis/Mega Drive': {
-        'fileName': 'genesis_mega_drive',
-        'url': 'https://www.geckocodes.org/index.php?chid=M&r=*&l=all'
-    },
-    'NeoGeo': {
-        'fileName': 'neo_geo',
-        'url': 'https://www.geckocodes.org/index.php?chid=E&r=*&l=all'
-    },
-    'Commodore 64': {
-        'fileName': 'commodore_64',
-        'url': 'https://www.geckocodes.org/index.php?chid=C&r=*&l=all'
-    },
-    'MSX': {
-        'fileName': 'msx',
-        'url': 'https://www.geckocodes.org/index.php?chid=X&r=*&l=all'
-    },
-    'TurboGraFX-16': {
-        'fileName': 'turbo_gra_f_x_16',
-        'url': 'https://www.geckocodes.org/index.php?chid=P&r=*&l=all'
-    },
-    'TurboGraFX-CD': {
-        'fileName': 'turbo_gra_f_x_c_d',
-        'url': 'https://www.geckocodes.org/index.php?chid=Q&r=*&l=all'
-    },
+    # 'Gamecube': {
+    #     'fileName': 'gamecube',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=G&r=*&l=all'
+    # },
+    # 'NES / Famicom': {
+    #     'fileName': 'n_e_s_famicom',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=F&r=*&l=all'
+    # },
+    # 'Super NES/Famicom': {
+    #     'fileName': 'super_n_e_s_famicom',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=J&r=*&l=all'
+    # },
+    # 'Nintendo 64': {
+    #     'fileName': 'nintendo_64',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=N&r=*&l=all'
+    # },
+    # 'Sega Master System': {
+    #     'fileName': 'sega_master_system',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=L&r=*&l=all'
+    # },
+    # 'Genesis/Mega Drive': {
+    #     'fileName': 'genesis_mega_drive',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=M&r=*&l=all'
+    # },
+    # 'NeoGeo': {
+    #     'fileName': 'neo_geo',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=E&r=*&l=all'
+    # },
+    # 'Commodore 64': {
+    #     'fileName': 'commodore_64',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=C&r=*&l=all'
+    # },
+    # 'MSX': {
+    #     'fileName': 'msx',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=X&r=*&l=all'
+    # },
+    # 'TurboGraFX-16': {
+    #     'fileName': 'turbo_gra_f_x_16',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=P&r=*&l=all'
+    # },
+    # 'TurboGraFX-CD': {
+    #     'fileName': 'turbo_gra_f_x_c_d',
+    #     'url': 'https://www.geckocodes.org/index.php?chid=Q&r=*&l=all'
+    # },
 };
 
 txtDownloadUrl = "https://www.geckocodes.org/txt.php?txt=";
 
 for url in urls:
     # Connect to the URL
-    response = requests.get(urls[url]['url']);
+    response = urllib.request.urlopen(urls[url]['url']).read();
 
     # Parse HTML and save to BeautifulSoup object
-    soup = BeautifulSoup(response.text, "html.parser");
+    soup = BeautifulSoup(response, "html.parser");
 
     # Create a temporary directory
     tempDirectory = tempfile.TemporaryDirectory();
