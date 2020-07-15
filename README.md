3480eaefea3f1522ec0dac14d93d8eb96eb5560e
Thomas <guitaristtom@users.noreply.github.com>
2020-07-15 03:32:31 -0700

    Added BS4 to the readme required library list. Added the various different systems that are available at GeckoCodes. Put the bottom part in to a forloop to iterate through each system.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ GeckoCodes Scraper
 ------------------
 
 Simple scraper that you can either run manually or run scheduled with something like CRON.
+
+### Required Libraries:
+* BeautifulSoup4
--- scrape.py
+++ scrape.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python
 
 import requests;
-import urllib.request;
 import time;
 from bs4 import BeautifulSoup;
 
 # URL to scrape
-url = "https://www.geckocodes.org/index.php?chid=R&r=*&l=all";
+# CHIDs:
+# R: Wii
+# W: WiiWare
+# D: Virtual Console Arcade
+# H: Wii Channels
+# G: Gamecube
+# F: NES / Famicom
+# J: Super NES/Famicom
+# N: Nintendo 64
+# L: Sega Master System
+# M: Genesis/Mega Drive
+# E: NeoGeo
+# C: Commodore 64
+# X: MSX
+# P: TurboGraFX-16
+# Q: TurboGraFX-CD
 
-# Connect to the URL
-response = requests.get(url);
+urls = {
+    'Wii': {'folder': 'wii', 'url': 'https://www.geckocodes.org/index.php?chid=R&r=*&l=all'},
+    'WiiWare': {'folder': 'wii_ware', 'url': 'https://www.geckocodes.org/index.php?chid=W&r=*&l=all'},
+    'Virtual Console Arcade': {'folder': 'virtual_console_arcade', 'url': 'https://www.geckocodes.org/index.php?chid=D&r=*&l=all'},
+    'Wii Channels': {'folder': 'wii_channels', 'url': 'https://www.geckocodes.org/index.php?chid=H&r=*&l=all'},
+    'Gamecube': {'folder': 'gamecube', 'url': 'https://www.geckocodes.org/index.php?chid=G&r=*&l=all'},
+    'NES / Famicom': {'folder': 'n_e_s_famicom', 'url': 'https://www.geckocodes.org/index.php?chid=F&r=*&l=all'},
+    'Super NES/Famicom': {'folder': 'super_n_e_s_famicom', 'url': 'https://www.geckocodes.org/index.php?chid=J&r=*&l=all'},
+    'Nintendo 64': {'folder': 'nintendo_64', 'url': 'https://www.geckocodes.org/index.php?chid=N&r=*&l=all'},
+    'Sega Master System': {'folder': 'sega_master_system', 'url': 'https://www.geckocodes.org/index.php?chid=L&r=*&l=all'},
+    'Genesis/Mega Drive': {'folder': 'genesis_mega_drive', 'url': 'https://www.geckocodes.org/index.php?chid=M&r=*&l=all'},
+    'NeoGeo': {'folder': 'neo_geo', 'url': 'https://www.geckocodes.org/index.php?chid=E&r=*&l=all'},
+    'Commodore 64': {'folder': 'commodore_64', 'url': 'https://www.geckocodes.org/index.php?chid=C&r=*&l=all'},
+    'MSX': {'folder': 'msx', 'url': 'https://www.geckocodes.org/index.php?chid=X&r=*&l=all'},
+    'TurboGraFX-16': {'folder': 'turbo_gra_f_x_16', 'url': 'https://www.geckocodes.org/index.php?chid=P&r=*&l=all'},
+    'TurboGraFX-CD': {'folder': 'turbo_gra_f_x_c_d', 'url': 'https://www.geckocodes.org/index.php?chid=Q&r=*&l=all'},
+};
 
-# Parse HTML and save to BeautifulSoup object
-soup = BeautifulSoup(response.text, "html.parser");
+for url in urls:
+    # Connect to the URL
+    response = requests.get(urls[url]['url']);
 
-for link in soup.find("div", class_="list").findAll("a"):
-    print(link.get('href'))
-    time.sleep(0.5);
+    # Parse HTML and save to BeautifulSoup object
+    soup = BeautifulSoup(response.text, "html.parser");
+
+    for link in soup.find("div", class_="list").findAll("a"):
+        # print(link.get('href'))
+        print(link.get('title'))
+        time.sleep(0.5); # For testing
